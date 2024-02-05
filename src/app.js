9c6f31a4dfaca77c0a2b3d567458a2f246329c69
Anar Bayramov <anar.bayram@yahoo.com>
2024-02-05 21:14:58 +0400

    configured database & defined initial product model

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
-const app =  express();
+const sequelize = require('./utils/database');
 
+const app =  express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
@@ -10,6 +11,16 @@ app.get('/', (req, res) => {
     return res.status(200).json({"message": "Welcome to Book store!"});
 })
 
-app.listen(PORT, () => {
-    console.log(`app is listening on port ${PORT}. Index page: http://localhost:${PORT}`);
-})
+sequelize.authenticate()
+    .then(connection => {
+        console.log('Connected to the database...')
+        return connection.sync();
+    })
+    .then(result => {
+        app.listen(PORT, () => {
+            console.log(`app is listening on port ${PORT}`);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
---
+++ src/models/products.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../utils/database');
+
+const Product = sequelize.define('product', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL(8, 2),
+        allowNull: false
+    }
+})
+
+module.exports = Product;
