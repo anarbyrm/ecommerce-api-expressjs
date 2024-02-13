c28b3af7eb2f6e883f9a2c47e5857721fc84c8a3
Anar Bayramov <anar.bayram@yahoo.com>
2024-02-13 21:10:53 +0400

    added cart item list

--- src/controllers/carts.js
+++ src/controllers/carts.js
@@ -82,9 +82,26 @@ const removeFromCart = async (req, res) => {
     }
 }
 
+const getCart = async (req, res) => {
+    try {
+        const cart = await req.user.getCart();
+        const cartItems = await cart.getProducts();
 
+        res.status(200).json({
+            message: 'success',
+            data: cartItems
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: 'error',
+            detail: err 
+        })
+    }
+}
 
 module.exports = {
     addToCart,
-    removeFromCart
+    removeFromCart,
+    getCart
 }
